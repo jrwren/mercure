f076b79fa19e037207616c661766dce043a22d23
Romain Lespinasse <romain.lespinasse@gmail.com>
2019-10-13 21:45:04 +0200

    Update cloudfunction.go

--- cloudfunction.go
+++ cloudfunction.go
@@ -9,12 +9,14 @@ import (
 )
 
 var (
-    startTime = time.Date(2019, time.October, 14, 0, 0, 0, 0, time.UTC)
-    endTime  = time.Date(2020, time.April, 14, 0, 0, 0, 0, time.UTC)
+    previousEndTime  = time.Date(2019, time.April, 14, 0, 0, 0, 0, time.UTC)
+    currentStartTime = time.Date(2019, time.October, 14, 0, 0, 0, 0, time.UTC)
+    currentEndTime  = time.Date(2020, time.April, 14, 0, 0, 0, 0, time.UTC)
+    nextStartTime = time.Date(2020, time.October, 14, 0, 0, 0, 0, time.UTC)
 )
 
 func IsItTheWinterBreakYet(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "{\"is_winter_break_yet\":\"" + strconv.FormatBool(isWinterBreakYet()) + "\", \"winter_break\": {\"start_time\":\"" + startTime.Format(time.RFC3339) + "\", \"end_time\":\"" + endTime.Format(time.RFC3339) + "\"}}")
+    fmt.Fprint(w, "{\"is_winter_break_yet\":\"" + strconv.FormatBool(isWinterBreakYet()) + "\", \"previous_winter_break\": {\"end_time\":\"" + previousEndTime.Format(time.RFC3339) + "\"}, \"current_winter_break\": {\"start_time\":\"" + currentStartTime.Format(time.RFC3339) + "\", \"end_time\":\"" + currentEndTime.Format(time.RFC3339) + "\"}, \"next_winter_break\": {\"start_time\":\"" + nextStartTime.Format(time.RFC3339) + "\"}}")
 }
 
 func isWinterBreakYet() bool {
